d5337b76db1be1cf1040a8d4c7b99fe4c62a8bbc
Milton Bezerra das Neves Neto <m.neto@academico.ifrn.edu.br>
2024-08-05 18:09:04 -0300

    implemetd

--- src/middleware/jwtMiddleware.js
+++ src/middleware/jwtMiddleware.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken"
 
 const verifyToken = (req, res, next) => {
     try {
+        const INDICE_TOKEN = 1
         const token = req.headers.authorization.split("")[1]
         const decoded = jwt.verify(token, process.env.SECRET)
         req.userId = decoded
